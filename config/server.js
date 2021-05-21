42fa37cfd45126d3837abf508d9844ff204c0529
imanol309 <imanolguzman309@gmail.com>
2021-05-21 17:01:26 -0400

    pimera version de strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a081f91c9589db94fd3149cbbb003e3d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0b814c25-090d-4202-a754-96253cca9759'
+};
